473a97fb45521fd28816d1251ad1456faee9bb40
sohelahamed23 <120620480+sohelahamed23@users.noreply.github.com>
2022-12-15 13:14:05 +0530

    Add files via upload

---
+++ Multiplication_table.java
@@ -0,0 +1,34 @@
+package project_1;
+import java.util.Scanner;
+public class Multiplication_table
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		byte project_1;
+    {
+		       Scanner sc = new Scanner(System.in); 
+		       int  number,a,b ;
+		       sc=new Scanner(System.in);
+		       System.out.println("enter the number:");
+		       number=sc.nextInt();
+		       for (a=number;a<=number;a++) 
+		       {
+		    	  for (b=1;b<=10;b++)
+		    	  {
+		    		   System.out.println(a+"*"+b+"="+a*b);
+		    	  
+		          }
+		       
+		       }
+		    	  
+		      
+		       
+	}
+
+	}
+
+	}
+
+
