0cb2d6224e3bafe911fb9b9feeba9559184c7c99
sohelahamed23 <120620480+sohelahamed23@users.noreply.github.com>
2022-12-15 13:14:39 +0530

    Add files via upload

---
+++ alphabet_checking.java
@@ -0,0 +1,26 @@
+package project_1;
+	import java.util.Scanner;
+
+	public class alphabet_checking {
+	    public static void main(String[] args) {
+
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("Enter any caracter : ");
+	        char ch = scanner.next().charAt(0);
+
+	        if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+
+	             System.out.println(ch + " is A ALPHABET.");
+
+	        } else if(ch >= '0' && ch <= '9') {
+
+	             System.out.println(ch + " is A DIGIT.");
+
+	        } else {
+
+	             System.out.println(ch + " is A SPECIAL CHARACTER.");
+	        }
+	    
+	    }
+	}
